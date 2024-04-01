3876f255d66e26a36f3a797f3558035a6ad8ab0c
guilhermealmeida123 <105080670+guilhermealmeida123@users.noreply.github.com>
2024-04-01 10:27:12 -0300

    ok

--- main.js
+++ main.js
@@ -27,4 +27,8 @@ let horas = Math.floor(minutos/60);
 let dias = Math.floor(horas/24);
 
 
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+
     contadores[0].textContent = dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
